f75a171d77cc3f28debbdda8036f81d6c7f292c1
Victor Herrera <victocode007@gmail.com>
2024-08-17 14:26:50 -0400

    productosJs

--- main.js
+++ main.js
@@ -1,3 +1,107 @@
-function updateCartCount(){
-    
-}
+const products =[
+    {
+        id: "game-01",
+        name: "The Legend Of Zelda: TOTK",
+        image: "./img/productos/juegos/product_ZeldaTotk.png",
+        description: "The Legend of Zelda: Tears of the Kingdom is an action-adventure game developed",
+        category: {
+            name: "videoGames",
+            id: "videoGames"
+        },
+        price: 56.98,
+    },
+    {
+        id: "game-02",
+        name: "Forza Horazon 5",
+        image: "./img/productos/juegos/product__ForzaHorizon5",
+        description: "Forza Horizon 5 is an action-adventure game developed by Playground Games",
+        category: {
+            name: "videoGames",
+            id: "videoGames"
+        },
+        price: 56.98,
+    },
+    {
+        id: "game-03",
+        name: "Horizon Forbiden West",
+        image: "./img/productos/juegos/product_horizonFw.png",
+        description: "Horizon: Forbiden West is an action-adventure game developed by Guerrilla Games",
+        category: {
+            name: "videoGames",
+            id: "videoGames"
+        },
+        price: 56.98,
+    },
+    {
+        id: "game-04",
+        name: "Assassins Creed: Valhalla",
+        img: "./img/productos/juegos/produtc__AC_Valhalla.webp",
+        description: "Assassins Creed: Valhalla is an action-adventure game developed by Ubisoft",
+        category: {
+            name: "videoGames",
+            id: "videoGames"
+        },
+        price: 56.98,
+    },
+    {
+        id: "game-05",
+        name: "Stars Wars: Fallen Order",
+        img: "./img/productos/juegos/product__star_wars-FO.jpeg",
+        description:"Stars Wars: Fallen Order is an action-adventure game developed by Electronics Art",
+        category: {
+            name: "videoGames",
+            id: "videoGames"
+        },
+        price: 56.98,
+    },
+    {
+        id: "console-01",
+        name: "Xbox One Series X",
+        img: "./img/productos/consolas/xbox.png",
+        category:{
+            name: "consolas",
+            id: "consolas"
+        },
+        price: 569.99
+    },
+    {
+        id: "console-02",
+        name: "PlayStation 5: blue edition",
+        img: "./img/productos/consolas/ps5_blue.png",
+        category:{
+            name: "consolas",
+            id: "consolas"
+        },
+        price: 569.99
+    },
+    {
+        id: "console-03",
+        name: "Nintendo Swicth",
+        img:"./img/productos/consolas/flagship.png",
+        category:{
+            name: "consolas",
+            id: "consolas"
+        },
+        price: 297.99,
+    },
+    {
+        id: "console-04",
+        name: "Nintendo Swicth: TOTK Edition",
+        img:"./img/productos/consolas/switchOled__especialEdition Totk.png",
+        category:{
+            name: "consolas",
+            id: "consolas"
+        },
+        price: 354.99,
+    },
+    {
+        id: "console-05",
+        name: "Nintendo Swicth: OLED Edition",
+        img:"./img/productos/consolas/switchOled.png",
+        category:{
+            name: "consolas",
+            id: "consolas"
+        },
+        price: 309.99,
+    },
+]
