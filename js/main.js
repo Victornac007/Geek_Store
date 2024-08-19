1afc24a7961e55042dda1d7c4f34007290be0cfc
Victor Herrera <victocode007@gmail.com>
2024-08-18 20:04:40 -0400

    mejorado del index js

--- js/main.js
+++ js/main.js
@@ -23,7 +23,7 @@ const products =[
     },
     {
         id: "game-03",
-        name: "Horizon Forbiden West",
+        name: "Horizon Forbidden West",
         img: "./img/productos/juegos/product_horizonFw.png",
         description: "Horizon: Forbiden West is an action-adventure game developed by Guerrilla Games",
         category: {
@@ -59,8 +59,8 @@ const products =[
         name: "Xbox One Series X",
         img: "./img/productos/consolas/xbox.png",
         category:{
-            name: "consolas",
-            id: "consolas"
+            name: "consoles",
+            id: "consoles"
         },
         price: 569.99
     },
@@ -69,8 +69,8 @@ const products =[
         name: "PlayStation 5: blue edition",
         img: "./img/productos/consolas/ps5_blue.png",
         category:{
-            name: "consolas",
-            id: "consolas"
+            name: "consoles",
+            id: "consoles"
         },
         price: 569.99
     },
@@ -79,8 +79,8 @@ const products =[
         name: "Nintendo Swicth",
         img:"./img/productos/consolas/flagship.png",
         category:{
-            name: "consolas",
-            id: "consolas"
+            name: "consoles",
+            id: "consoles"
         },
         price: 297.99,
     },
@@ -89,8 +89,8 @@ const products =[
         name: "Nintendo Swicth: TOTK Edition",
         img:"./img/productos/consolas/switchOled__especialEdition Totk.png",
         category:{
-            name: "consolas",
-            id: "consolas"
+            name: "consoles",
+            id: "consoles"
         },
         price: 354.99,
     },
@@ -99,51 +99,98 @@ const products =[
         name: "Nintendo Swicth: OLED Edition",
         img:"./img/productos/consolas/switchOled.png",
         category:{
-            name: "consolas",
-            id: "consolas"
+            name: "consoles",
+            id: "consoles"
         },
         price: 309.99,
     },
 
-]
+];
 const allProductsContainer = document.querySelector("#products__container")
 const categoryButtoms = document.querySelectorAll(".category-buttom")
+let addButtoms = document.querySelectorAll(".add-btn")
+const countCart = document.querySelector("#cart-count")
 
-function cargarProducto(){
-    products.forEach(producto => {
+function cargarProducto(productsChoiced){
+
+    allProductsContainer.innerHTML = "";
+    
+    productsChoiced.forEach(product => {
         
         const div = document.createElement("div");
         div.classList.add("product__card");
         div.innerHTML = `
             <div class="product__card--img">
-                <img src="${producto.img}" alt="${producto.name}">
+                <img src="${product.img}" alt="${product.name}">
             </div>
             <div class="card__info">
-                <h3 class="product__card--title">${producto.name}</h3>
+                <h3 class="product__card--title">${product.name}</h3>
                 <hr class="card-divider">
                 <div class="card-footer">
                     <div class="card-price">
-                        <span class="card__info--price">$</span>${producto.price ? producto.price : 'Precio no disponible'}
+                        <span class="card__info--price">$</span>${product.price ? product.price : 'Precio no disponible'}
                     </div>
-                    <i class="bi bi-cart-plus-fill" style="font-size: 1.5rem; color: rgb(44, 73, 127);" ></i>
+                    <button class="add-btn" id="${product.id}">
+                        Agregar
+                        <i class="bi bi-cart-plus-fill" style="font-size: 1.5rem; color: rgb(44, 73, 127);" ></i>
+                    </button>
                 </div>
             </div>
             `
 
         allProductsContainer.appendChild(div);
     });
+    updateAddButtoms()
 }
 
 categoryButtoms.forEach(buttom => {
     buttom.addEventListener("click", (e) => {
-        categoryButtoms.forEach(buttom => buttom.classList.remove("nav__button-active"))
 
-    e.currentTarget.classList.add("nav__button-active")
+        categoryButtoms.forEach(buttom => buttom.classList.remove("nav__button-active"))
+        e.currentTarget.classList.add("nav__button-active")
+
+        if(e.currentTarget.id != "showAll"){
+            const categoryProducts = products.filter(product => product.category.id === e.currentTarget.id);
+            cargarProducto(categoryProducts);
+        } else{
+            cargarProducto(products);
+        }
+        
     })
 } )
 
 
+function updateAddButtoms(){
+    addButtoms = document.querySelectorAll(".add-btn");
+
+    addButtoms.forEach(buttom => {
+        buttom.addEventListener("click", addToCart)
+    });
+}
 
 document.addEventListener("DOMContentLoaded", () => {
-    cargarProducto();
+    cargarProducto(products);
 });
+
+const productsInCart = [];
+
+function addToCart (e){
+    const idButtom = e.currentTarget.id;
+    const productAdded = products.find(product => product.id === idButtom);
+
+    if(productsInCart.some(product => product.id === idButtom)){
+        const index = productsInCart.findIndex(product => product.id === idButtom)
+        productsInCart[index].quantify ++;
+    } else {
+        productAdded.quantify = 1;
+        productsInCart.push(productAdded)
+    }
+    updateCartCount()
+
+    localStorage.setItem("products-In-Cart", JSON.stringify(productsInCart))
+}
+function updateCartCount(){
+    let newCount = productsInCart.reduce((acc, product) => acc + product.quantify, 0)
+    countCart.innerHTML = newCount
+    console.log(newCount);
+}
